a6be43fa41a025946a6395b876ba84cfac398122
Sivamedisetti <96729473+Sivamedisetti@users.noreply.github.com>
2022-05-21 14:24:36 +0530

    def hcf(a,b):     if a>b:         a,b=b,a     c=a     while c!=0:         if a%c==0 and b%c==0:             return c         c-=1 a,b=map(int,input().split()) print(hcf(a,b))

---
+++ GCD_or_HCF.py
@@ -0,0 +1,10 @@
+def hcf(a,b):
+    if a>b:
+        a,b=b,a
+    c=a
+    while c!=0:
+        if a%c==0 and b%c==0:
+            return c
+        c-=1
+a,b=map(int,input().split())
+print(hcf(a,b))
