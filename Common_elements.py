ed04a601008a27e1434fc6442b4816f5f4ea2400
Sivamedisetti <96729473+Sivamedisetti@users.noreply.github.com>
2022-05-28 23:13:46 +0530

    x,y=map(int,input().split()) l=list(map(int,input().split())) m=list(map(int,input().split())) for i in sorted(set(l),key=l.index):     if m.count(i)!=0:         print(i,end=" ")

---
+++ Common_elements.py
@@ -0,0 +1,6 @@
+x,y=map(int,input().split())
+l=list(map(int,input().split()))
+m=list(map(int,input().split()))
+for i in sorted(set(l),key=l.index):
+    if m.count(i)!=0:
+        print(i,end=" ")
