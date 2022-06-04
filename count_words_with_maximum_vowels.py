cfbe80dfc5aa93098eee46d8d0a0db6b9166ca6f
Sivamedisetti <96729473+Sivamedisetti@users.noreply.github.com>
2022-06-04 15:20:33 +0530

    def vowel(n):     c=0     for i in n:         if i in "aeiou":             c+=1     return c l=list(map(str,input().split())) k=[] for i in l:     k.append(vowel(i)) print(k.count(max(k)))

---
+++ count_words_with_maximum_vowels.py
@@ -0,0 +1,11 @@
+def vowel(n):
+    c=0
+    for i in n:
+        if i in "aeiou":
+            c+=1
+    return c
+l=list(map(str,input().split()))
+k=[]
+for i in l:
+    k.append(vowel(i))
+print(k.count(max(k)))
