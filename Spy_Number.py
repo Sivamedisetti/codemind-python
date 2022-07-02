a324fb0dbe15601eac932679d094a08fd7595cf4
Sivamedisetti <96729473+Sivamedisetti@users.noreply.github.com>
2022-07-02 20:48:59 +0530

    n=input() s,p=0,1 for i  in n:     i=int(i)     s+=i     p*=i #print(s,p) if s==p:     print("Spy Number") else:     print("Not Spy Number")

--- Spy_Number.py
+++ Spy_Number.py
@@ -1,12 +1,12 @@
-s=0
-p=1
-n=int(input())
-while n!=0:
-    x=n%10
-    n=n//10
-    s+=x
-    p*=x
+n=input()
+s,p=0,1
+for i  in n:
+    i=int(i)
+    s+=i
+    p*=i
+#print(s,p)
 if s==p:
     print("Spy Number")
 else:
     print("Not Spy Number")
+    
