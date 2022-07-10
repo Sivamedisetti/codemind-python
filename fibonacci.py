8b32f6007db3b76e4317269aabdef611d0972feb
Sivamedisetti <96729473+Sivamedisetti@users.noreply.github.com>
2022-07-10 09:45:55 +0530

    def fib(x):     a = 0     b = 1     if x==0:         return 1     c = a+b     while c<x:         c = a + b         a = b         b = c     if x==c:         return 1     else:         return 0 n=int(input()) print("0 1",end=" ") c=3 i=1 while(1):     if fib(i):         print(i,end=" ")         c+=1         if c>n:             break     i+=1

--- fibonacci.py
+++ fibonacci.py
@@ -1,7 +1,27 @@
-def fib(n):
-    if n<=1:
-        return n
-    return fib(n-1)+fib(n-2)
+def fib(x):
+    a = 0
+    b = 1
+    if x==0:
+        return 1
+    c = a+b
+    while c<x:
+        c = a + b
+        a = b 
+        b = c
+    if x==c:
+        return 1
+    else:
+        return 0
 n=int(input())
-for i in range(n):
-    print(fib(i),end=" ")
+print("0 1",end=" ")
+c=3
+i=1
+while(1):
+    if fib(i):
+        print(i,end=" ")
+        c+=1
+        if c>n:
+            break
+    i+=1
+    
+    
