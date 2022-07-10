b2d4e2f4d96be6808959562d4eade974a042b991
Sivamedisetti <96729473+Sivamedisetti@users.noreply.github.com>
2022-07-10 11:51:41 +0530

    n=int(input()) l=list(map(int,input().split())) c=0 for i in range(1,n-1,2):     if l[i-1]>l[i]<l[i+1] or l[i-1]<l[i]>l[i+1]:         c+=1     else:         print("no")         c=0         break if c!=0:     print("yes")

--- Wave_array.py
+++ Wave_array.py
@@ -1,14 +1,12 @@
 n=int(input())
 l=list(map(int,input().split()))
-c=1
+c=0
 for i in range(1,n-1,2):
     if l[i-1]>l[i]<l[i+1] or l[i-1]<l[i]>l[i+1]:
-        pass
+        c+=1
     else:
+        print("no")
         c=0
         break
-if c==0:
-    print("no")
-else:
+if c!=0:
     print("yes")
-    
