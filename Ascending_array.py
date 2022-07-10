f4973c547d877def21472b5565becd29efb48fb4
Sivamedisetti <96729473+Sivamedisetti@users.noreply.github.com>
2022-07-10 11:54:54 +0530

    n=int(input()) l=list(map(int,input().split())) c=0 for i in range(1,n):     if  l[i-1]<l[i]:         c+=1     else:         print("no")         c=0         break if c!=0:     print("yes")

--- Ascending_array.py
+++ Ascending_array.py
@@ -2,11 +2,11 @@ n=int(input())
 l=list(map(int,input().split()))
 c=0
 for i in range(1,n):
-    if l[i-1]<l[i]:
-        pass
+    if  l[i-1]<l[i]:
+        c+=1
     else:
         print("no")
-        c=1
+        c=0
         break
-if c==0:
+if c!=0:
     print("yes")
