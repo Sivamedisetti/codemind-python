ac42bd510d0190cd5b3662209a2f3f4c337bd142
Sivamedisetti <96729473+Sivamedisetti@users.noreply.github.com>
2022-07-11 17:22:33 +0530

    n=int(input()) l=list(map(int,input().split())) m=[] for i in sorted(set(l),key=l.index):     if i==l.count(i):         m.append(i) if m==[]:     print("-1") else:     print(*m)

--- Elements_same_as_count.py
+++ Elements_same_as_count.py
@@ -1,9 +1,11 @@
-x = int(input())
-l = list(map(int,input().split()))
-c=0
+n=int(input())
+l=list(map(int,input().split()))
+m=[]
 for i in sorted(set(l),key=l.index):
-    if l.count(i)==i:
-        print(i,end=" ")
-        c=1
-if c==0:
+    if i==l.count(i):
+        m.append(i)
+if m==[]:
     print("-1")
+else:
+    print(*m)
+    
