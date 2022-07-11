f7ef43ac630ec0d05a83e1fbac91399591b0e08f
Sivamedisetti <96729473+Sivamedisetti@users.noreply.github.com>
2022-07-11 16:50:14 +0530

    n=int(input()) l=list(map(int,input().split())) k=int(input()) c=0 for i in l:     if i<=k:         c+=i print(c)

--- sum_of_array__elements_upto__Kth__number_in_an_array.py
+++ sum_of_array__elements_upto__Kth__number_in_an_array.py
@@ -1,11 +1,8 @@
-x = int(input())
-l = list(map(int,input().split()))
-a=int(input())
-k=[]
-for i in range(x):
-    if i<a:
-        k.append(l[i])
-if k==[]:
-    print('-1')
-else:
-    print(sum(k))
+n=int(input())
+l=list(map(int,input().split()))
+k=int(input())
+c=0
+for i in l:
+    if i<=k:
+        c+=i
+print(c)
